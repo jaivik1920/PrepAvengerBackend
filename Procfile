2b7d9b858151300ced708385e268a3f22114d51b
jaivik1920 <pateljaivik2025@gmail.com>
2021-11-03 16:32:42 +0530

    Solving heroku error

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar build/libs/PrepAvenger-0.0.1-SNAPSHOT.jar
--- src/main/java/com/prepavenger/controller/PrepAvengerController.java
+++ src/main/java/com/prepavenger/controller/PrepAvengerController.java
@@ -3,10 +3,10 @@ package com.prepavenger.controller;
 import java.util.List;
 
 
+
 import java.util.Optional;
 import java.util.Random;
 
-import org.aspectj.weaver.ast.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
